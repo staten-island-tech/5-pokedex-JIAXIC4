79f3bb0e402ed8c736520f4d83401cd71a692d6f
JIAXIC4 <JIAXIC4@NYCSTUDENTS.NET>
2026-03-05 09:58:30 -0500

    test

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 def testcheck(n,q,a):
     rect = 0
 
-    for x in range(q):
+    for x in range(len(q)):
         if q[x] == a[x]:
             rect = rect + 1
 
+
     print(rect)
 
-
-testcheck = (3, "ABC", "ABC")
+testcheck(3,"ABC","CBA")
