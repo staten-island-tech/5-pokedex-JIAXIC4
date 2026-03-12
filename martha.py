dfc0af7c7517e160d2e440e665892265f693be64
JIAXIC4 <JIAXIC4@NYCSTUDENTS.NET>
2026-03-12 10:07:58 -0400

    finished test jiaxi justin
    100 yay

--- martha.py
+++ martha.py
@@ -1,21 +1,21 @@
-def martha_gamble(x):
+def martha_gamble(money,first,second,third):
     total_time= 0
-    first = 0
-    second = 0
-    third = 0
+    first = first
+    second = second
+    third = third
     position = 0
-    money = 0
+    money = money
 
-    while position == 0:
-        first += 1
-        money = money - 1
-        total_time += 1 
-        position = position +1
-        if first == 35:
-            money = money + 30
-            first = 0 
-
-        if position == 1:
+    while money > 0:
+        if position == 0:
+            first += 1
+            money = money - 1
+            total_time += 1 
+            position = position +1
+            if first == 35:
+                money = money + 30
+                first = 0 
+        elif position == 1:
             second += 1
             money = money - 1
             total_time += 1 
@@ -23,12 +23,11 @@ def martha_gamble(x):
             if second == 100:
                 money = money + 60
                 second = 0 
-
-        if position == 2:
+        elif position == 2:
             third += 1
             money = money - 1
             total_time += 1 
-            position = position +1
+            position = 0
             if third == 10:
                 money = money + 9
                 third = 0 
@@ -37,4 +36,4 @@ def martha_gamble(x):
 
     print(total_time)
         
-martha_gamble(1005)
+martha_gamble(77,4,9,3)
